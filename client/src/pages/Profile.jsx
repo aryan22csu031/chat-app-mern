30b6ce8514b337cfb7e938cffcd48a1b0655340b
aryan22csu031 <aryan22csu031@ncuindia.edu>
2024-12-26 09:59:19 +0530

    profile page updated

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -33,7 +33,7 @@ const Profile = () => {
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
-                src={selectedImg || authUser.profilePic || "/avatar.png"}
+                src={selectedImg || authUser.profilePic || "/avatar.jpeg"}
                 alt="Profile"
                 className="size-32 rounded-full object-cover border-4 "
               />
